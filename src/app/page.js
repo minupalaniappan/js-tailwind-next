874c5f1262a35f0ba2fd7a6c3224f6a284c8ec8e
Minu Palaniappan <minupal6@gmail.com>
2024-10-06 20:00:51 -0400

    Add

--- src/app/page.js
+++ src/app/page.js
@@ -10,6 +10,7 @@
  */
 
 import { tw } from "@minupalaniappan/brise";
+import get from "lodash/get";
 
 const Title = tw.div`
   text-xl
@@ -19,7 +20,19 @@ const Title = tw.div`
 `;
 
 const Page = () => {
-  return <Title>Hello world!</Title>;
+  const fetchAsync = async () => {
+    return fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((e) => e.json())
+      .then((e) => console.info(e))
+      .catch((e) => console.error(e));
+  };
+
+  return (
+    <div>
+      <Title>Hello world!</Title>
+      <button onClick={async () => await fetchAsync()} />
+    </div>
+  );
 };
 
 export default Page;
